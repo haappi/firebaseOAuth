7af83ff28e3892e94980bc6efb4b1a6f42ca0884
haappi <74637670+haappi@users.noreply.github.com>
2023-11-18 16:17:49 -0600

    cool!

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from fastapi import APIRouter, Depends, Cookie
+from fastapi import APIRouter, Depends, Cookie, HTTPException
 from google.auth.exceptions import InvalidValue
 from starlette.requests import Request
 from starlette.responses import RedirectResponse, Response
@@ -19,20 +19,33 @@ async def get_current_user(response: Response, request: Request, jwt: str = Cook
         return RedirectResponse(url=f"/school/oauth/login", status_code=307)
 
     try:
-        jwt = await verify_google_jwt(decrypt_secret(jwt), os.getenv("GOOGLE_CLIENT_ID"))
+        jwt: dict = await verify_google_jwt(decrypt_secret(jwt), os.getenv("GOOGLE_CLIENT_ID"))
     except InvalidValue:
         await refresh_users_token(request, decrypt_secret(request.cookies.get("refresh_token")), response)
 
-    return jwt
+    return await User.retrieve_user(jwt['sub'])
+
+
+async def is_admin(current_user: User = Depends(get_current_user)):
+    if not current_user.admin:
+        raise HTTPException(status_code=403, detail="Forbidden")
+    return current_user
 
 
 @router.get("/dashboard")
-async def test(response: Response, request: Request, current_user: dict | RedirectResponse = Depends(get_current_user)):
+async def test(response: Response, request: Request, current_user: User | RedirectResponse = Depends(get_current_user)):
     if isinstance(current_user, RedirectResponse):
         return current_user
     print(current_user)
     # Your route logic here
-    user = await User.retrieve_user(current_user['sub'])
-    print(user)
-    return {"message": "You are authenticated!", "user": user.model_dump()}
+    print(current_user)
+    return {"message": "You are authenticated!", "user": current_user.model_dump()}
 
+@router.get("/admin")
+async def admin(response: Response, request: Request, current_user: User | RedirectResponse = Depends(is_admin)):
+    if isinstance(current_user, RedirectResponse):
+        return current_user
+    print(current_user)
+    # Your route logic here
+    print(current_user)
+    return {"message": "You are authenticated as an admin!", "user": current_user.model_dump()}
