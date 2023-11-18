aaf7f016c50f47dbf9e4f54b2716cfbb08b914d8
haappi <74637670+haappi@users.noreply.github.com>
2023-11-18 17:39:57 -0600

    change image

--- Backend/routes/dashboard.py
+++ Backend/routes/dashboard.py
@@ -27,7 +27,7 @@ from starlette.responses import RedirectResponse, Response
 
 from backend.POPO.Secrets import Secrets
 from backend.POPO.User import User
-from backend.utils import verify_google_jwt, decrypt_secret, refresh_users_token
+from backend.utils import decrypt_secret, verify_google_jwt, refresh_users_token
 
 load_dotenv()
 
